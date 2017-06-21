e46fcb58c793f858989a187d2452bd0a65412465
Andy Roberts <andyr8939@gmail.com>
2017-06-21 12:08:24 +1200

    First commit of public scripts to master branch

---
+++ Get-BackupExecMediaRemovalEmail.ps1
@@ -0,0 +1,36 @@
+﻿# Created by Andy Roberts (andyr8939@gmail.com)
+#
+# This script was created so I knew which backups tapes needed to be removed from our autoloader each day, ready to be sent off-site for secure storage.
+# Without this I had to manually open Backup Exec and qeury the media slots for most recently used media.
+# This was testing on Backup Exec 2014 running on Windows Server 2012 with a HP 1/8 G2 LT05 Autoloader
+
+# Define contacts
+$recipient = enter receipient email here
+$sender = enter sender email address here
+$subject = "Enter email subject header"
+$smtpserver = enter your email server here
+
+# Import BEMCLI Modules
+Import-Module "D:\Program Files\Symantec\Backup Exec\Modules\BEMCLI"
+
+# If you run this job automatically after a backup job completes, you need to make it wait for the media to be put back into the slots, otherwise it doesn't show.
+# If you however are running this as a scheduled task then leave this commented out.
+#Start-Sleep 60
+
+# Get the tapes most recently used, set at 72hrs to cover weekend backups
+$lastdate=(get-date).AddHours(-72)
+
+# Get the current media in the library
+Get-BEMedia -MediaVault "Online Tape Media"
+
+# Identity which media in the library was allocated in the last time period
+$mediatoremove = Get-BEMedia -MediaVault "Online Tape Media" | where {$_.AllocatedDate -gt $lastdate} 
+
+#Ceate the mail body by defining a variable of looping through all the meida found and format as a string so can be used in email.
+$body=@"
+The following are the tapes to be removed today.
+$($mediatoremove | foreach { Get-BERoboticLibrarySlot -Media $_.Name } | select Name, Media | Sort-Object Name | Out-String)
+"@
+
+#Send the mail
+Send-MailMessage -To $recipient -Subject $subject -From $sender -Body $body -SmtpServer $smtpserver
---
+++ Get-VeeamBackupStats.ps1
@@ -0,0 +1,43 @@
+﻿# Created by Andy Roberts (andyr8939@gmail.com)
+#
+# This script was created so I can export Veeam backup stats to an Excel file which was required by an internal reporting process.
+# This was then picked up by another software agent running on the Veeam VM and transfered outw
+# I run this as a scheduled task every day at 7am so all the backups have completed by this time, but you could always run it as a post Veeam backup task.
+
+# Add the VeeamPSSnapIn
+Add-PSSnapin VeeamPSSnapIn
+
+# Declare an array to collect our result objects
+$resultsarray =@()
+
+# Get the Veeam Backup Jobs
+$Jobs = Get-VBRJob
+
+# Cycle through the backup jobs
+foreach ($Job in $Jobs) {
+    # Get the Last Backup Session
+	$LastSession = $Job.FindLastSession()
+    # Now get the invididual tasks within that Session
+	$TaskSessions = $LastSession | Get-VBRTaskSession
+	foreach ($vm in $TaskSessions) { 
+            # Create a new custom object to hold our result.
+            $jobObject = new-object PSObject
+            # Now put each item you need into the object
+            $jobObject | add-member -membertype NoteProperty -name "VM ID" -Value ($VMID = $vm.ID)
+            $jobObject | add-member -membertype NoteProperty -name "Job ID" -Value ($JobID = $vm.JobSessId)
+            $jobObject | add-member -membertype NoteProperty -name "Job Name" -Value ($JobName = $vm.Jobname)
+            $jobObject | add-member -membertype NoteProperty -name "VM Name" -Value ($VMName = $vm.Name)
+            $jobObject | add-member -membertype NoteProperty -name "Status" -Value ($Status = $vm.Info.Status)
+            $jobObject | add-member -membertype NoteProperty -name "Start Time" -Value ($StartTime = $vm.JobSess.CreationTime)
+            $jobObject | add-member -membertype NoteProperty -name "End Time" -Value ($EndTime = $vm.JobSess.EndTime)
+            $jobObject | add-member -membertype NoteProperty -name "Elapsed Time - Seconds" -Value ($ElapsedTime = (New-TimeSpan -Start $vm.JobSess.CreationTime -End $vm.JobSess.EndTime).Seconds)
+            $jobObject | add-member -membertype NoteProperty -name "Backup Job Size - Gb" -Value ($BackupSize = [math]::Round((($vm.JobSess.BackupStats.BackupSize) / 1GB),2))
+            $jobObject | add-member -membertype NoteProperty -name "Backup Job Data Size - Gb" -Value ($BackupDataSize = [math]::Round((($vm.JobSess.BackupStats.DataSize) / 1GB),2))
+            $jobObject | add-member -membertype NoteProperty -name "Backup Job DeDupe Ratio - %" -Value ($BackupDeDupSize = $vm.JobSess.BackupStats.DeDupRatio)
+	
+# Save the current $jobObject by appending it to $resultsArray
+$resultsarray += $jobObject
+    }
+}
+# Export the array to a CSV
+$resultsarray | Export-csv "$tempdir\veeam.csv" –notypeinformation
