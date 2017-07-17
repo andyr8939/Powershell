2db81a3c58a6bf14e54183dd048d4613c345f12e
Andy Roberts <andyr8939@gmail.com>
2017-07-17 13:51:23 +1200

    Parameterized the AD User Lockout Script

--- Get-ADUserLockoutLocation.ps1
+++ Get-ADUserLockoutLocation.ps1
@@ -1,47 +1,104 @@
-﻿# Created by Andy Roberts (andyr8939@gmail.com)
-# This script was created due to dealing with users who constantly lock themselves out when in a VDI environment by leaving open sessions.
-# It will query all available DC event logs looking for the last set of bad passwords attempts and also the last locked out location reported
-# on each DC for that user, allowing you to find where they are getting locked from.
+﻿<#
+.SYNOPSIS
+Find out where an Active Directory User Account is being locked out from
+.DESCRIPTION
+Get-ADUserLockoutLocation will search through Domain Controller Security logs to identify a specific users last location location
+as well as where their last incorrect password came from to help idenfity the location
+Created by Andy Roberts (andyr8939@gmail.com)
+.PARAMETER username
+The username of the user you are searching for.  Mandatory
+.PARAMETER dcou
+The OU in your Active Directory that contains your domain controllers.
+Haven't figured out how to dynamically query this yet.  Change this!
+.PARAMETER adserver
+A domain controller to query.  
+Default - Your current logon server
+.PARAMETER failedpasswords
+How manny failed locations to search against.
+Default - 5
+.PARAMETER credential
+Elevated Credentials to search against DC Event Logs.  Only needed if account is different to current.
+.PARAMETER failedpassid
+EventID for the Failed Password attempt.
+.PARAMETER lockoutid
+EventID for the Account Lockout.
+.EXAMPLE
+Get-ADUserLockoutLocation -SearchName username Andy -AdServer dc1 -credential (Get-Credential)
+This searchs for user Andy, using credentials that I'm prompted for with my DC being dc1
+.EXAMPLE
+Get-ADUserLockoutLocation -SearchName username Andy
+This searchs for user Andy, using your current logon credentials and using your current logon server as the DC.
+#>
+[CmdletBinding()]
+param (
+[Parameter(Mandatory=$True,HelpMessage="Enter a Username to search on")]
+[string]$username,
 
-# Get the username you want to search for
-$searchname = Read-Host -Prompt "What username do you need?"
+[Parameter(HelpMessage="Enter a DC OU to search")]
+[string]$dcou = "OU=Domain Controllers,DC=changeme,DC=changeme",
 
-# Until I can figure out how to dynamically search for the OU based on domain if you are not domain joined, this has to be specified
-$dcou = "OU=Domain Controllers,DC=changeme,DC=changme"
+[Parameter(HelpMessage="Enter an Domain Controller Name to search")]
+[string]$adserver = $env:LOGONSERVER.Trim("\\"),
 
-# Get AD server if not joined to that domain.  If bypassed with enter take the local value, searching on 0 is because its zero length string
-# and then trim out the \\ so it can be used in the Get-ADComputer section
-Write-Host "If you are quering a domain you are not joined to, enter a DC name below.  Otherwise just press enter to take your local DC."
-$adserver = Read-Host -Prompt "Enter DC name"
-if ($adserver -le 0 )
-{    $env:LOGONSERVER.Trim("\\")  }
+[Parameter(HelpMessage="Number of failed passwords to search")]
+[int]$failedpasswords = 5,
 
-# Get Credentials to access the event logs on DCs.  You won't need this if domain joined, so can also remove the credential checks elsewhere
-# but I need to jump arond domains so have left this in.
-Write-Host "Enter your domain credentials to query DC event logs"
-$mycreds = Get-Credential
+[Parameter(HelpMessage="Enter your credentials if different than current")]
+[System.Management.Automation.PSCredential]
+[System.Management.Automation.Credential()]
+$credential = [System.Management.Automation.PSCredential]::Empty
 
-# Get DCs for the domain
-# Get only 2008 and higher DCs due to lack of powershell on 2003
-$dcs = (Get-ADComputer -SearchBase $dcou -Filter { OperatingSystem -notlike '*Windows Server 2003' } -Credential $mycreds -Server $adserver).Name
+#[Parameter(HelpMessage="EventID for Failed Password")]
+#[int]$failedpassid = "4771",
 
-# Specify the amount of failed password attempts you want to seach for in the event logs
-$failedpasswords = 5
+#[Parameter(HelpMessage="EventID for Account Lockout")]
+#[int]$lockoutid = "4740"
+)
+
 
 # Get failed attempts
 
 Write-Host ""
 Write-Host "These systems are where the last" $failedpasswords "failed password attempts came from"
-Invoke-command -ComputerName $dcs {get-eventlog Security -EntryType FailureAudit -InstanceId 4771 -ErrorAction SilentlyContinue -WarningAction SilentlyContinue} -Credential $mycreds | 
-    where {$_.ReplacementStrings[0] -like $searchname} | 
+
+if ($credential -ne [System.Management.Automation.PSCredential]::Empty)
+{
+    # Get DCs for the domain
+    # Get only 2008 and higher DCs due to lack of powershell on 2003
+    $dcs = (Get-ADComputer -SearchBase $dcou -Filter { OperatingSystem -notlike '*Windows Server 2003' } -Credential $credential -Server $adserver).Name
+    Invoke-command -ComputerName $dcs {get-eventlog Security -EntryType FailureAudit -InstanceId 4771 -ErrorAction SilentlyContinue -WarningAction SilentlyContinue} -Credential $credential | 
+    where {$_.ReplacementStrings[0] -like $username} | 
         select @{l="userid";e={$_.ReplacementStrings[0]}},@{l="ip";e={$_.ReplacementStrings[6]}},Index,EventID,TimeWritten,@{l="Domain Controller";e={$_.PSComputerName}} -First $failedpasswords | 
             FT -AutoSize
+}
+else
+{
+    # Get DCs for the domain
+    # Get only 2008 and higher DCs due to lack of powershell on 2003
+    $dcs = (Get-ADComputer -SearchBase $dcou -Filter { OperatingSystem -notlike '*Windows Server 2003' } -Server $adserver).Name
+    Invoke-command -ComputerName $dcs {get-eventlog Security -EntryType FailureAudit -InstanceId 4771 -ErrorAction SilentlyContinue -WarningAction SilentlyContinue} | 
+    where {$_.ReplacementStrings[0] -like $username} | 
+        select @{l="userid";e={$_.ReplacementStrings[0]}},@{l="ip";e={$_.ReplacementStrings[6]}},Index,EventID,TimeWritten,@{l="Domain Controller";e={$_.PSComputerName}} -First $failedpasswords | 
+            FT -AutoSize
+}
 
 
 # Get locked out status
 
 Write-Host "This is the system where the user account lockout came from"
-Invoke-Command -ComputerName $dcs {get-eventlog Security -EntryType SuccessAudit -InstanceId 4740 -Newest 1 -ErrorAction SilentlyContinue -WarningAction SilentlyContinue} -Credential $mycreds | 
-    select @{l="Username";e={$_.ReplacementStrings[0]}},@{l="Locked Out System";e={$_.ReplacementStrings[1]}},@{l="Lockout Time";e={$_.TimeWritten}},@{l="Domain Controller";e={$_.PSComputerName}} | 
-        FT -AutoSize
 
+
+if ($credential -ne [System.Management.Automation.PSCredential]::Empty)
+{
+    Invoke-Command -ComputerName $dcs {get-eventlog Security -EntryType SuccessAudit -InstanceId 4740 -Newest 1 -ErrorAction SilentlyContinue -WarningAction SilentlyContinue} -Credential $credential |
+    where {$_.ReplacementStrings[0] -like $username} | 
+        select @{l="Username";e={$_.ReplacementStrings[0]}},@{l="Locked Out System";e={$_.ReplacementStrings[1]}},@{l="Lockout Time";e={$_.TimeWritten}},@{l="Domain Controller";e={$_.PSComputerName}} | 
+            FT -AutoSize
+}
+else
+{
+    Invoke-Command -ComputerName $dcs {get-eventlog Security -EntryType SuccessAudit -InstanceId 4740 -Newest 1 -ErrorAction SilentlyContinue -WarningAction SilentlyContinue} |
+    where {$_.ReplacementStrings[0] -like $username} |
+        select @{l="Username";e={$_.ReplacementStrings[0]}},@{l="Locked Out System";e={$_.ReplacementStrings[1]}},@{l="Lockout Time";e={$_.TimeWritten}},@{l="Domain Controller";e={$_.PSComputerName}} | 
+            FT -AutoSize
+}
